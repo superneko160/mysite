83b3d2ecbe60c9ae120663dc9ef3298753e6549a
sneko <supernekocat31@gmail.com>
2023-12-02 01:26:57 +0900

    add timeline .

--- src/src/app/components/history.tsx
+++ src/src/app/components/history.tsx
@@ -4,10 +4,39 @@ export default function History() {
       <div className="bg-sky-950 rounded p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <div className="text-gray-50 font-bold text-xl mb-2">History</div>
-          <ul>
-            <li className="text-gray-50">2999.01.01 My birthday.</li>
-            <li className="text-gray-50">2999.12.31 R.I.P</li>
-          </ul>
+          <div className="relative w-1/2 m-8">
+            <div className="border-r-2 border-gray-50 absolute h-full top-0 left-3.5"></div>
+            <ul className="list-none m-0 p-0">
+              <li className="mb-2">
+                <div className="flex items-center mb-1">
+                  <div className="bg-gray-50 rounded-full h-8 w-8"></div>
+                  <div className="flex-1 ml-4 font-bold">Aug 1992</div>
+                </div>
+                <div className="ml-12">I was born.</div>
+              </li>
+              <li className="mb-2">
+                <div className="flex items-center mb-1">
+                  <div className="bg-gray-50 rounded-full h-8 w-8"></div>
+                  <div className="flex-1 ml-4 font-bold">Apr 2015</div>
+                </div>
+                <div className="ml-12">First programming.</div>
+              </li>
+              <li className="mb-2">
+                <div className="flex items-center mb-1">
+                  <div className="bg-gray-50 rounded-full h-8 w-8"></div>
+                  <div className="flex-1 ml-4 font-bold">Apr 2017</div>
+                </div>
+                <div className="ml-12">Joined a software development company.</div>
+              </li>
+              <li className="mb-2">
+                <div className="flex items-center mb-1">
+                  <div className="bg-gray-50 rounded-full h-8 w-8"></div>
+                  <div className="flex-1 ml-4 font-bold">Jul 2999</div>
+                </div>
+                <div className="ml-12">R.I.P</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
