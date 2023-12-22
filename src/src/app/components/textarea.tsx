327b230d46a6ce7c4bd2f5a7f8383da943ab4e0a
sneko <supernekocat31@gmail.com>
2023-12-22 23:14:36 +0900

    fix components design.

--- src/src/app/components/textarea.tsx
+++ src/src/app/components/textarea.tsx
@@ -21,7 +21,7 @@ export default function Textarea({
 
   return (
     <textarea
-      className="block h-52 p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+      className="block h-52 p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 resize-none"
       placeholder={placeholder}
       required
       name={name}
