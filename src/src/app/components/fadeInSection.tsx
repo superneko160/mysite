7f47bce728707d2586ba0465889d88c432bd8beb
sneko <supernekocat31@gmail.com>
2025-05-11 17:43:45 +0900

    style: format code for consistency in FadeInSection component

--- src/src/app/components/fadeInSection.tsx
+++ src/src/app/components/fadeInSection.tsx
@@ -1,22 +1,22 @@
 import * as motion from "motion/react-client";
 
 type FadeInSectionProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 export default function FadeInSection({ children }: FadeInSectionProps) {
-    return (
-        <motion.div
-            viewport={{ once: true }}
-            initial={{ opacity: 0, x: 50 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{
-                delay: 0.2,
-                duration: 0.8,
-                ease: "easeOut"
-            }}
-        >
-            {children}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      viewport={{ once: true }}
+      initial={{ opacity: 0, x: 50 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{
+        delay: 0.2,
+        duration: 0.8,
+        ease: "easeOut",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
 }
