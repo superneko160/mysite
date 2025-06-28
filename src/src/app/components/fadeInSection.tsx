e02cfdf6ec5ada1019311b413ac2ea5bd5aab742
sneko <supernekocat31@gmail.com>
2025-06-28 19:20:06 +0900

    fix: fix motion configuration  to FadeInSection component
    その他の変更
    
    - バージョンを1.2.7に更新
    - page.tsxから不要なインポートを削除
    - skillコンポーネントにスキルのアイコンを追加

--- src/src/app/components/fadeInSection.tsx
+++ src/src/app/components/fadeInSection.tsx
@@ -8,10 +8,10 @@ export default function FadeInSection({ children }: FadeInSectionProps) {
   return (
     <motion.div
       viewport={{ once: true }}
-      initial={{ opacity: 0, x: 50 }}
-      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
       transition={{
-        delay: 0.2,
+        delay: 0.5,
         duration: 0.8,
         ease: "easeOut",
       }}
--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import * as motion from "motion/react-client";
 
 import type { HistoryData } from "./types/HistoryData/";
 import ThemeToggle from "./components/themeToggle";
